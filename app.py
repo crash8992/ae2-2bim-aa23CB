31ebbc4eea35abc737ec333e08b0ad4d9ab522fc
Cristian Bravo <105746182+crash8992@users.noreply.github.com>
2023-07-08 19:04:41 -0500

    carga deber semana 12

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-"""
-    Ejemplo del manejo de hilos
-"""
-
 import requests
 import time
 import csv
 import threading
-# librería de python que permite ejecutar comandos
-import subprocess
+import os
 
 def obtener_data():
     lista = []
     with open("informacion/data.csv") as archivo:
         lineas = csv.reader(archivo, quotechar="|")
         for row in lineas:
-            # pass
-            # lista.append((numero, pagina))
-    # se retorna la lista con la información que se necesita
+            print(row[0].split("|"))
+            num = row[0].split("|")[0]
+            pag = row[0].split("|")[1]
+            lista.append((num, pag))  
     return lista
 
-def worker(numero, url):
+def worker(num, url):
     print("Iniciando %s %s" % (threading.current_thread().getName(), url ))
-    # pass
+    pag = requests.get(url)
+    archivo_path = os.path.join("salida", f"{num}.txt")
+    os.makedirs(os.path.dirname(archivo_path), exist_ok=True)
+    with open(archivo_path, "w") as archivo:
+        archivo.write(pag.text)
     time.sleep(10)
     print("Finalizando %s" % (threading.current_thread().getName()))
 
 for c in obtener_data():
     # Se crea los hilos
     # en la función
-    numero = c[0]
+    num = c[0]
     url = c[1]
     hilo1 = threading.Thread(name='navegando...',
                             target=worker,
-                            args=(numero, url))
+                            args=(num, url))
     hilo1.start()
